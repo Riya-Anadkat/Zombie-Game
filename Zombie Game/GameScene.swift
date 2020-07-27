8274158e7b1d6050f0170448706997c03970bdec
Riya-Anadkat <riya.anadkat@gmail.com>
2020-07-27 17:36:53 -0400

    Commit message

--- Zombie Game/GameScene.swift
+++ Zombie Game/GameScene.swift
@@ -29,7 +29,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     //    func random () -> CGFloat {
-    //        return CGFloat(Float(arc4random()) / 0xFFFFFFFFF)
+    //        return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
     //    }
     func random(min: CGFloat, max: CGFloat) -> CGFloat {
         //return random() * (max - min) + min
